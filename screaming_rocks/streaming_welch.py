ca0dfba82746ca3c64054f08a519c7673641cb90
Tim Lingard <tklingard@gmail.com>
2020-03-05 14:14:12 +0000

    bugfixes multiprocessing in streaming_welch, improves context management

--- screaming_rocks/streaming_welch.py
+++ screaming_rocks/streaming_welch.py
@@ -1,8 +1,18 @@
-from dask_file_reading import read_batch
-from partWelch import partWelch
+from .dask_file_reading import read_batch
+from .partWelch import partWelch
 from multiprocessing import Pool
 from tqdm import tqdm
 import numpy as np
+
+
+def __task(start_time, sensor_id, window_size, fs, data_folder):
+    data = read_batch(
+        sensor_id,
+        start_time,
+        window_size,
+        data_folder=data_folder
+    )
+    return partWelch(data, fs)
 
 
 def streaming_welch(
@@ -11,45 +21,38 @@ def streaming_welch(
     end_time,
     window_size=0.001,
     fs=1e7,
-    data_folder=''
+    cpu_count=4,
+    data_folder='',
 ):
+    """MOTION TO RENAME TO SQWELCH WAS OVERRULED"""
     start_times = np.arange(start_time, end_time, window_size / 2)
-    pbar = tqdm(total=len(start_times))
     counter = 0
     freq = None
     psd_running_sum = None
 
-    def callback(freq_in, psd_in):
-        nonlocal counter
-        nonlocal pbar
-        nonlocal psd_running_sum
-        nonlocal freq
-        if psd_running_sum is None:
-            psd_running_sum = psd_in
-            freq = freq_in
-        else:
-            psd_running_sum += psd_in
-        counter += 1
-        pbar.update(counter)
+    with tqdm(total=len(start_times)) as pbar, Pool(cpu_count) as pool:
 
-    def task(start_time):
-        data = read_batch(
-            sensor_id,
-            start_time,
-            window_size,
-            data_folder=data_folder
-        )
-        return partWelch(data, fs)
+        def callback(results_in):
+            freq_in, psd_in = results_in
+            nonlocal counter
+            nonlocal pbar
+            nonlocal psd_running_sum
+            nonlocal freq
+            if psd_running_sum is None:
+                psd_running_sum = psd_in
+                freq = freq_in
+            else:
+                psd_running_sum += psd_in
+            counter += 1
+            pbar.update(1)
 
-    cpu_count = 4
-    pool = Pool(cpu_count)
-    pool.apply_async(
-        task,
-        args=start_times,
-        callback=callback
-    )
-    pool.close()
-    pool.join()
-    pbar.close()
+        for start_time in start_times:
+            pool.apply_async(
+                __task,
+                args=(start_time, sensor_id, window_size, fs, data_folder),
+                callback=callback
+            )
+        pool.close()
+        pool.join()
     psd_avg = psd_running_sum / counter
-    return psd_avg
+    return freq, psd_avg
